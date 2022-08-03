b64b1eb1c5a032bfa912bbc0e5613f5c51932c68
Jerwin <jerwinsauco@gmail.com>
2022-08-03 17:53:26 -0400

    updated header with local time and statement

--- assets/script.js
+++ assets/script.js
@@ -1,9 +1,17 @@
+// Date from moment.js in UTC 
+var nowDate = moment.utc().format("LLLL");
+console.log(nowDate, "- now in UTC"); 
+// Date converted to local time (user's time zone)
+var localNowDate = moment.utc(nowDate).local().format('[Today is] dddd' + ' ' + 'LL');
+// Attention to coder, this is the author's time zone (EST)
+console.log(localNowDate, "- UTC now to EST"); 
 
-var nowTime = moment.utc().format("LLLL");
-console.log(nowTime, "- now in UTC"); 
+document.querySelector("#currentDay").textContent = localNowDate;
 
+var nowTime = moment.utc().format("HH:mm");
+console.log(nowTime, "- now in UTC");
 
-var localNowTime = moment.utc(nowTime).local().format('[Today is] dddd' + ' ' + 'LL');
-console.log(localNowTime, "- UTC now to EST"); 
+var localNowTime = moment.utc(nowDate).local().format('[Time is now]' + ' ' + 'HH:mm');
+console.log("The time is now", localNowTime)
 
-document.querySelector("#currentDay").textContent = localNowTime;
+document.querySelector("#currentTime").textContent = localNowTime;
