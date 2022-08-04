1fe58acaae45c41f140c9e912f4bd7d7d490ad95
Jerwin <jerwinsauco@gmail.com>
2022-08-04 12:38:38 -0400

    inputText and saveBtn function added

--- assets/script.js
+++ assets/script.js
@@ -1,10 +1,11 @@
+// DATE and TIME Section
 // Date from moment.js in UTC 
 var nowDate = moment.utc().format("LLLL");
-console.log(nowDate, "- now in UTC"); 
+console.log(nowDate, "- now in UTC");
 // Date converted to local time (user's time zone)
 var localNowDate = moment.utc(nowDate).local().format('[Today is] dddd' + ' ' + 'LL');
-// Attention to coder, this is the author's time zone (EST)
-console.log(localNowDate, "- UTC now to EST"); 
+// Attention to coder, author's time zone in EST
+console.log(localNowDate, "- UTC now to EST");
 
 document.querySelector("#currentDay").textContent = localNowDate;
 
@@ -15,3 +16,25 @@ var localNowTime = moment.utc(nowDate).local().format('[Time is now]' + ' ' + 'H
 console.log("The time is now", localNowTime)
 
 document.querySelector("#currentTime").textContent = localNowTime;
+// END DATE and TIME Section
+
+// jQuery ready() function executes only when the DOM is fully loaded. 
+// $(document).ready(function () {
+    $(".saveIcon").on("click", function () {
+        var inputValue = $(this).siblings(".description").val();
+        var time = $(this).parent().attr("id");
+        // inputValue by user is stored in localStorage
+        localStorage.setItem(time, inputValue);
+    })
+}
+// Loads the text from local storage to the input fields
+$("#8 .description").val(localStorage.getItem("8"));
+$("#9 .description").val(localStorage.getItem("9"));
+$("#10 .description").val(localStorage.getItem("10"));
+$("#11 .description").val(localStorage.getItem("11"));
+$("#12 .description").val(localStorage.getItem("12"));
+$("#13 .description").val(localStorage.getItem("13"));
+$("#14 .description").val(localStorage.getItem("14"));
+$("#15 .description").val(localStorage.getItem("15"));
+$("#16 .description").val(localStorage.getItem("16"));
+$("#17 .description").val(localStorage.getItem("17"));
